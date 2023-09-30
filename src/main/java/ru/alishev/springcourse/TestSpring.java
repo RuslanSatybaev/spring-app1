dd03f120f66b6819ef477ac89e2ddcd660ce5114
Ruslan Satybaev <medtehnik850@gmail.com>
2023-10-01 00:08:03 +0700

    Lesson8: Factory method

--- src/main/java/ru/alishev/springcourse/TestSpring.java
+++ src/main/java/ru/alishev/springcourse/TestSpring.java
@@ -7,15 +7,9 @@ public class TestSpring {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 "applicationContext.xml"
         );
-        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        MusicPlayer musicPlayer2 = context.getBean("musicPlayer", MusicPlayer.class);
-        System.out.println(musicPlayer == musicPlayer2);
-        System.out.println(musicPlayer);
-        System.out.println(musicPlayer2);
-        musicPlayer.setVolume(100);
-        System.out.println(musicPlayer2.getVolume());
 
-        musicPlayer.playMusicList();
+        ClassicalMusic music = context.getBean("musicBean", ClassicalMusic.class);
+        System.out.println(music.getSong());
 
         context.close();
     }
